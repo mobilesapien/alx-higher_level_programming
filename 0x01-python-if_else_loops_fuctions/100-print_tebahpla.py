9e3612a8f117f8dbbbe14a7e673a4b17a42855d9
Hakeem Olatunji <hkmolatunji@gmail.com>
2022-12-01 14:37:22 -0800

    removed deleted files from git

--- 0x01-python-if_else_loops_fuctions/100-print_tebahpla.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-for x in range(122, 96, -1):
-    if (x % 2) == 0:
-        letter = x
-    else:
-        letter = x - 32
-    print("{}".format(chr(letter)), end='')
--- 0x01-python-if_else_loops_fuctions/101-remove_char_at.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    if n >= 0:
-        copystr = str[:n] + str[n + 1:]
-    else:
-        copystr = str[:]
-    return (copystr)
